4163dfe6c0ad62c04af63b971a067cbf532fddf4
Lucas David Fernandez <1933.lucas.fernandez@gmail.com>
2024-02-28 08:33:01 -0300

    Majority of the UI splited in components

---
+++ src/lib/components/Logout.svelte
@@ -0,0 +1,7 @@
+<form method="POST" action="?/logout">
+	<button
+		class="bg-primary-100 text-primary-700 hover:bg-primary-accent-100 focus:bg-primary-accent-100 active:bg-primary-accent-200 inline-block rounded px-14 pb-6 pt-6 text-center text-xs font-extrabold uppercase leading-normal transition duration-150 ease-in-out focus:outline-none focus:ring-0"
+		data-te-ripple-init
+		data-te-ripple-color="light">LogOut</button
+	>
+</form>
