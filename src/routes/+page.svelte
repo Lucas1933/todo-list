25164f6c73796faacf09e52a2e016a897caf57a4
Lucas David Fernandez <1933.lucas.fernandez@gmail.com>
2024-03-04 07:01:39 -0300

    add welcome page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+	import logo from 'assets/icons/favicon.png';
+</script>
+
+<div class="flex h-full flex-col items-center justify-center">
+	<h1 class="mb-4 text-center text-5xl">Welcome to the simple todo list</h1>
+	<img class="w-40" src={logo} alt="" />
+	<div class="mt-4">
+		<a href="auth/login">
+			<button
+				class=" dark:active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]] bg-primary hover:bg-primary-600 focus:bg-primary-600 active:bg-primary-700 inline-block w-full rounded px-12 pb-2 pt-2.5 text-base font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_#3b71ca] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.3),0_4px_18px_0_rgba(59,113,202,0.2)] dark:shadow-[0_4px_9px_-4px_rgba(59,113,202,0.5)] dark:hover:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)] dark:focus:shadow-[0_8px_9px_-4px_rgba(59,113,202,0.2),0_4px_18px_0_rgba(59,113,202,0.1)]"
+				data-te-ripple-init
+				data-te-ripple-color="light"
+			>
+				Start
+			</button></a
+		>
+	</div>
+</div>
