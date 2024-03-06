d39459444e28d1d2c31644d6b3aa83933aee06d2
Lucas David Fernandez <1933.lucas.fernandez@gmail.com>
2024-03-06 06:35:17 -0300

    add +error.svelte for 404 page not found

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import pageNotFoundIcon from 'assets/icons/cross-window-svgrepo-com.svg';
+</script>
+
+<div class="flex h-full w-full flex-col items-center justify-center">
+	<img class="w-36" src={pageNotFoundIcon} alt="" />
+	<h1 class="mt-5 font-extrabold">PAGE NOT FOUND</h1>
+
+	<a href="/" class="text-primary-500 mt-10 text-xl underline">Go to home page</a>
+</div>
